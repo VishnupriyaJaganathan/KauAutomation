3256c43a919a11cf69242101e08053d9b355e1b0
Vishnupriya Jaganathan <126341574+VishnupriyaJaganathan@users.noreply.github.com>
2026-03-25 21:15:43 +0100

    Update GoogleSearchTests.java

--- selenium-template-java-master/selenium-template-java-master/src/test/java/tests/GoogleSearchTests.java
+++ selenium-template-java-master/selenium-template-java-master/src/test/java/tests/GoogleSearchTests.java
@@ -1,51 +1,85 @@
 package tests;
 
 import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
+
 import enums.Browser;
 import factory.BrowserProvider;
 import pages.GoogleHomePage;
 import pages.GoogleResultsPage;
 
-@Test
+/**
+ * TestNG tests for the KAU website.
+ * Updated to target kau.se, removed Thread.sleep, fixed bugs.
+ */
 public class GoogleSearchTests {
-	
-    private WebDriver driver;
-    GoogleHomePage googleHomePage;
 
-    /**
-     * This method will be executed before the test start.
-     */
+    private WebDriver driver;
+    private GoogleHomePage kauHomePage;
+
     @BeforeSuite
     public void initialize() {
         driver = BrowserProvider.createDriver(Browser.CHROME);
         driver.manage().window().maximize();
-        googleHomePage = new GoogleHomePage(driver);
-    }
-
-    /**
-     * This method perform a google search test. We can have multiple @Test methods inside this class.
-     */
-    @Test
-    public void googleSearchTest() {
         driver.get("https://www.kau.se/");
-        // Delay to ensure page elements are loaded, you can adjust the time as needed
-        try {
-            Thread.sleep(5000); // 5 seconds delay
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        googleHomePage.clickMittKau();
+        kauHomePage = new GoogleHomePage(driver);
+        kauHomePage.dismissCookieBanner();
     }
 
     /**
-     * This method will be executed at the end of the test.
+     * Test 1: Verify the KAU homepage title.
      */
+    @Test(priority = 1)
+    public void verifyHomepageTitle() {
+        Assert.assertTrue(
+            driver.getTitle().contains("Karlstads universitet"),
+            "Homepage title should contain 'Karlstads universitet'"
+        );
+    }
+
+    /**
+     * Test 2: Click Mitt Kau link and verify navigation.
+     */
+    @Test(priority = 2)
+    public void testMittKauNavigation() {
+        driver.get("https://www.kau.se/");
+        kauHomePage.dismissCookieBanner();
+        kauHomePage.clickMittKau();
+        Assert.assertTrue(
+            driver.getCurrentUrl().contains("kau.se"),
+            "Should navigate within kau.se after clicking Mitt Kau"
+        );
+    }
+
+    /**
+     * Test 3: Search for "IT" and verify results appear.
+     */
+    @Test(priority = 3)
+    public void testSearch() {
+        driver.get("https://www.kau.se/");
+        kauHomePage = new GoogleHomePage(driver);
+        kauHomePage.dismissCookieBanner();
+        GoogleResultsPage resultsPage = kauHomePage.searchFor("IT");
+        String resultText = resultsPage.getSearchResultText();
+        Assert.assertTrue(
+            resultText.contains("IT"),
+            "Search results page should mention 'IT'"
+        );
+    }
+
     @AfterSuite
     public void quitDriver() {
-        driver.quit();
-        driver = null;
+        if (driver != null) {
+            driver.quit();
+            driver = null;
+        }
     }
 }
+```
+
+Path:
+```
+src/test/java/tests/GoogleSearchTests.java
